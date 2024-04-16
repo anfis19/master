153dda1738f482c481b787e1f8ed431cfb58f424
Anton <anfis19@student.sdu.dk>
2024-04-16 10:46:47 +0200

    Added GMR

--- calinon/calinon.py
+++ calinon/calinon.py
@@ -34,7 +34,7 @@ m1_ori = m_s3
 
 m3_pos = m_s2 * m_e1
 m_time = m_e1 * m_e3
-m_t_s2 = m_e1 * m_s3
+m_t_s2 = m_e1 * m_s2
 
 class Calinon:
     def __init__(self, points):
@@ -66,16 +66,27 @@ class Calinon:
         # # print("Sigma", self.sigma[0])
         # # print("Mu: ", self.mean[0])¨
 
+
         g = rs.Gaussian(manifold).mle(self.points)
+        gmm = rs.GMM(manifold, 6)
+        gmm.kmeans(self.points)
+        lik,avglik = gmm.fit(self.points, reg_lambda=1e-2, maxsteps = 500)
         results = []
         i_in  = 0 # Input manifold index
         i_out = 1 # Output manifold index
         x_in = self.points
+        #x_in = self.points[0:200:10]
         # print(x_in)
-        results = g.condition(x_in,i_in=i_in,i_out=i_out)
-        for idx, r in enumerate(results):
-            self.sigma.append(r.sigma)
-            mu = (x_in[idx][0], r.mu)
+        # results = g.condition(x_in,i_in=i_in,i_out=i_out)
+        # for idx, r in enumerate(results):
+        #     self.sigma.append(r.sigma)
+        #     mu = (x_in[idx][0], r.mu)
+        #     self.mean.append(mu)
+        for idx, p in enumerate(list(self.points)):
+            results.append(gmm.gmr(p[i_in],i_in=i_in,i_out=i_out)[0])
+            self.sigma.append(results[idx].sigma)
+            print("Sigma", results[idx].sigma)
+            mu = (p[i_in], results[idx].mu)
             self.mean.append(mu)
 
     
@@ -113,24 +124,25 @@ class Calinon:
     def lqr_path(self):
         '''Computes LQR at each timestep on manifold
         '''
+        dim = 3
         # A = np.array([np.zeros(shape=(3,3)), np.zeros(shape=(3,3)), np.zeros(shape=(3,3)),np.zeros(shape=(3,3))]).reshape(6,6)
         # A[:3,3:] = np.identity(n=3,dtype=float)
         # B = np.array([np.zeros(shape=(3,3)), np.identity(n=3,dtype=float)]).reshape(6,3)
         # R = np.identity(n=3,dtype=float) * 0.01
-        A = np.array([np.zeros(shape=(2,2)), np.zeros(shape=(2,2)), np.zeros(shape=(2,2)),np.zeros(shape=(2,2))]).reshape(4,4)
-        A[:2,2:] = np.identity(n=2,dtype=float)
-        B = np.array([np.zeros(shape=(2,2)), np.identity(n=2,dtype=float)]).reshape(4,2)
-        R = np.identity(n=2,dtype=float) * 0.01
+        A = np.array([np.zeros(shape=(dim,dim)), np.zeros(shape=(dim,dim)), np.zeros(shape=(dim,dim)),np.zeros(shape=(dim,dim))]).reshape(2*dim,2*dim)
+        A[:dim,dim:] = np.identity(n=dim,dtype=float)
+        B = np.array([np.zeros(shape=(dim,dim)), np.identity(n=dim,dtype=float)]).reshape(2*dim,dim)
+        R = np.identity(n=dim,dtype=float) * 0.01
         Q = []
         for i in range(len(self.sigma)):
             tmp = np.array(self.sigma[i])
-            tmp = tmp[0:2,0:2]
+            tmp = tmp[0:dim,0:dim]
             #copy upper half and insert into lower half
             # upper = np.triu(tmp)
             # tmp = upper + upper.transpose() - np.diag(np.diag(tmp))
             # print(tmp)
             tmp = np.linalg.inv(tmp)
-            Z = np.zeros((2,2),dtype=int) # Create off-diagonal zeros array
+            Z = np.zeros((dim,dim),dtype=int) # Create off-diagonal zeros array
             tmp = np.asarray(np.bmat([[tmp, Z], [Z, tmp]]))
             Q.append(tmp)
 
@@ -144,18 +156,18 @@ class Calinon:
             k_temp, S, E = control.lqr(A, B, Q[i], R)
             # self.K.append(k_temp[0:3,0:3] * np.identity(n=3))
             # self.D.append(k_temp[0:3,3:6] * np.identity(n=3))
-            self.K.append(k_temp[0:2,0:2] * np.identity(n=2))
-            self.D.append(k_temp[0:2,2:4] * np.identity(n=2))
-            # print("Mean",self.mean[i])
-            # print("Point: ", self.points[i])
+            self.K.append(k_temp[0:dim,0:dim] * np.identity(n=dim))
+            self.D.append(k_temp[0:dim,dim:2*dim] * np.identity(n=dim))
+            print("Mean",self.mean[i])
+            print("Point: ", self.points[i])
             e = m_time.log(self.points[i], self.mean[i])
             # Q = np.eye(3)
-            e = e[1:].reshape(3,1)
-            e = e[0:2]
+            e = e[1:].reshape(dim,1)
+            # e = e[0:dim]
             self.u.append(-np.matmul(self.K[i],e))
+            # print("U: ", self.u[i])
         np.linalg.cholesky(self.K)
         np.linalg.cholesky(self.D)
-        print(self.u)
 
 
 
@@ -167,9 +179,9 @@ class Calinon:
 def main():
     startpts = np.array([0, 0, 0])
     endpts = np.array([12, 3, 8])
-    dems = pbddata.get_letter_dataS2(letter='S',n_samples=4,use_time=True)
+    dems = pbddata.get_letter_dataS2(letter='S',n_samples=1,use_time=True)
     data = [point for dem in dems for point in dem]
-    data = sorted(data, key=lambda x: x[0][0])
+    #data = sorted(data, key=lambda x: x[0][0])
     print(data[0])
     line = np.vstack(pbddata.get_letter_dataS2(letter='S',n_samples=4,use_time=False))
     time = np.linspace(0,1,800)
@@ -177,9 +189,10 @@ def main():
     line = (line, np.flip(line, 0), time.reshape(800,1))
     # print("Data", sorted(data, key=lambda x: x[0][0])[:10])
 
-    print("S2 Time: ", m_s2.id_elem, " NdimT: ", m_t_s2.n_dimT, " NdimM; ", m_t_s2.n_dimM)
-    print("M time: ", m_e3.id_elem, " NdimT: ", m_time.n_dimT, " NdimM; ", m_time.n_dimM)
+    #print("S2 Time: ", m_s2.id_elem, " NdimT: ", m_t_s2.n_dimT, " NdimM; ", m_t_s2.n_dimM)
+    #print("M time: ", m_e3.id_elem, " NdimT: ", m_time.n_dimT, " NdimM; ", m_time.n_dimM)
 
+    g = rs.Gaussian(m_time).mle(data)
     calinon = Calinon(data)
     calinon.create_gaussians(m_time)
     # print(calinon.get_mean(points, m_time,data[50]))
@@ -203,49 +216,63 @@ def main():
     T[3, :] = [0, 0, 0, 1]
 
     trajectory = []
-
     for i in range(len(calinon.mean)):
         T[:3, 3] = calinon.mean[i][1]
         damp = calinon.D[i]
         stiff = calinon.K[i]
         wrench = np.zeros(shape=(6,1))
-        wrench[0:3] = calinon.u[i]
+        #wrench[0:3] = calinon.u[i]
         rotatedWrench = wrench
         compliant = adm.controller(SE3(T), rotatedWrench, mass=mass, damp=damp, stiff=stiff)
         trajectory.append(compliant[:3, 3])
         # if i == 0:
         #     print(compliant)
     trajectory = np.array(trajectory)
+    # trajectory = np.array(calinon.u)
+    #fig = plt.figure()
 
-    fig = plt.figure()
- 
-    # syntax for 3-D projection
+    #syntax for 3-D projection
+    # plt.figure(figsize=(5,5) )
+    # ax = plt.subplot(111,projection='3d')
+    # s2_fcts.plot_manifold(ax)
+    # plt_data = m_t_s2.swapto_tupleoflist(data)
+    # plt.plot(plt_data[1][:,0],plt_data[1][:,1],plt_data[1][:,2],'.', label='Demonstration')     # Original Data
+    # plt_data = m_t_s2.swapto_tupleoflist(calinon.mean)
+    # plt.plot(plt_data[1][:,0],plt_data[1][:,1],plt_data[1][:,2],'.', label='Mean')     # Original Data
+    # label = 'Gaussian'
+    # #for r, s in zip(calinon.mean, calinon.sigma):
+    # #    s2_fcts.plot_gaussian(ax,r[1],s, showtangent=False,
+    # #                     linealpha=0.3,color='yellow',label=label)
+    # #    label=''
+    # plt.legend(); plt.show()
+
     ax = plt.axes(projection ='3d')
-    print("Data", np.array(list(map(lambda x: x[1], data))))
+    #print("Data", np.array(list(map(lambda x: x[1], data))))
     ax.plot3D(trajectory[:,0],trajectory[:,1],trajectory[:,2], 'red')
-    # trajectory = np.array(list(map(lambda x: x[1], data)))
-    # ax.plot3D(trajectory[:,0],trajectory[:,1],trajectory[:,2], 'green')
+    demo = np.array(list(map(lambda x: x[1], data)))
+    ax.plot3D(demo[:,0],demo[:,1],demo[:,2], 'blue')
+    ax.legend(['Manifold','Demonstration'])
     plt.show()
-    # return 0
     plt.plot(trajectory)
     plt.legend(['x','y','z'])
     plt.show()
     start = trajectory[0]
     start = SE3.Trans(start)
-    print(robot.ik_LM(start))
+    #print(robot.ik_LM(start))
     robot.q = robot.ik_LM(start)[0]#robot.ik_LM(start)
 
     Tep = SE3.Trans(0.6, -0.3, 0.1) * SE3.OA([0, 1, 0], [0, 0, -1])
     sol = robot.ik_LM(SE3.Trans(trajectory[-1]))         # solve IK
-    print(sol)
+    #print(sol)
     q_pickup = sol[0]
     qt = rtb.jtraj(robot.qr, q_pickup, 50)
     robot.plot(qt.q, backend='pyplot')
     dt = 0.05
     qt_test = []
     env = swift.Swift()
-    # env.launch(realtime=True)
-    arrived = False
+    #env.launch(realtime=True)
+    # arrived = False
+    # env.add(robot)
     # for i in range(len(trajectory)-1):
     #     arrived = False
     # while not arrived:
@@ -253,8 +280,8 @@ def main():
     #     v, arrived = rtb.p_servo(current_pos, SE3.Trans(trajectory[-1]), 1)
     #     print(np.linalg.pinv(robot.jacobe(robot.q)) @ v)
     #     robot.qd = np.linalg.pinv(robot.jacobe(robot.q)) @ v
-    # env.step(dt)
-        # qt_test.append(robot.q)
+    #     env.step(dt)
+    #     qt_test.append(robot.q)
     # print(robot.qd)
     # print(np.array(qt_test))
     # robot.plot(np.array(qt_test), backend='pyplot')
